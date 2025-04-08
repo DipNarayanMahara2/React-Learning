50ff17bfad216a0866855d8138e06129b9706228
DipNarayanMahara2 <dipnarayanmahara28@gmail.com>
2025-04-08 19:27:10 +0545

    react hooks and useState

--- src/About.jsx
+++ src/About.jsx
@@ -1,13 +1,41 @@
-import Button from "./Button"
+import { useState } from "react";
+import Button from "./Button";
+import "./App.css";
 
-function About(){
+function About() {
+  // const state =useState("Dipak")
+  // console.log(state)
+
+  // const state = useState(0);
+  // const firstState = state[0];
+  // const secondState = state[1];
+
+  const [count, setCount] = useState(0);
+  const increaseCount = () => {
+    setCount(count + 1);
+  };
+
+  const [decreaseCount, setDecreaseCount] = useState(20);
+  const DecreaseCount = () => {
+    setDecreaseCount(decreaseCount - 1);
+  };
+
   return (
     <>
       <h1>Hello this is About page</h1>
-      <Button name ="Learn More" />
+      <Button name="Learn More" />
+      <div className="style">
+        <h1>counter</h1>
+        <h1>{count}</h1>
+        <button onClick={increaseCount}>+</button>
+      </div>
+      <div className="style">
+        <h1>Decreasing Counter</h1>
+        <h1>{decreaseCount}</h1>
+        <button onClick={DecreaseCount}>-</button>
+      </div>
     </>
   );
 }
 
-
-export default About
+export default About;
